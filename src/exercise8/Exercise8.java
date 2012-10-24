4c10df516e38157446313725c1366a36d6accc7a
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-24 14:12:21 +0100

    Initial commit.

---
+++ src/exercise8/Exercise8.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise8;
+
+import BBK.PiJ01.common.BadInput;
+import BBK.PiJ01.common.Exercise;
+import BBK.PiJ01.common.ExerciseChooser;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Sam Wright <swrigh11@dcs.bbk.ac.uk>
+ */
+public class Exercise8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws BadInput {
+        ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+        
+        
+        ExerciseChooser ech = new ExerciseChooser(exercises);
+        ech.run();
+    }
+}
