ba3ffc38519a216e4b8ee30acf77cff07d4590b4
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-10-24 17:14:42 +0100

    Done all of q1.  Elements can be deleted from the tree, and the whole tree can be rebalanced.

--- src/exercise8/IntegerTreeNode.java
+++ src/exercise8/IntegerTreeNode.java
@@ -4,6 +4,12 @@
  */
 package exercise8;
 
+import BBK.PiJ01.common.IOGeneric;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author Sam Wright <swrigh11@dcs.bbk.ac.uk>
@@ -17,6 +23,8 @@ public class IntegerTreeNode {
     public IntegerTreeNode(int value) {
         this.value = value;
     }
+    
+    private IntegerTreeNode() {}
 
     public void add(int value) {
         if (value > this.value) {
@@ -25,7 +33,7 @@ public class IntegerTreeNode {
             } else {
                 right.add(value);
             }
-        } else {
+        } else if (value < this.value) {
             if (left == null) {
                 left = new IntegerTreeNode(value);
             } else {
@@ -43,11 +51,11 @@ public class IntegerTreeNode {
             if (value > this.value) {
                 return right.contains(value);
             }
-
-            return left.contains(value);
-        } catch (NullPointerException err) {
-            return false;
-        }
+            if (value < this.value) {
+                return left.contains(value);
+            }
+        } catch (NullPointerException err) {}
+        return false;
     }
     
     public int getMax() {
@@ -67,8 +75,130 @@ public class IntegerTreeNode {
     }
     
     public String toString() {
-        return String.format("%d L[%s] R[%s]", value, 
-                                        (left==null? "":left),
-                                        (right==null? "":right));
+        
+        return String.format("%d%s%s", value, 
+                                        (left==null? "":" L["+left+"]"),
+                                        (right==null? "":" R["+right+"]"));
+        
+        //return " [" + value + (left==null? "":left) + (right==null? "":right) + "]";
+        
+    }
+    
+    public int depth() {
+        if (isLeaf())
+            return 0;
+        if (left != null && right == null)
+            return left.depth() + 1;
+        if (left == null && right != null)
+            return right.depth() + 1;
+        return Math.max(left.depth(), right.depth()) + 1;
+    }
+    
+    public boolean isLeaf() {
+        return left == null && right == null;
+    }
+    
+    public IntegerTreeNode getParent(int value) {
+        try {
+            if (value > this.value) {
+                if (right.value == value)
+                    return right;
+                return right.getParent(value);
+            } else if (value < this.value) {
+                if (left.value == value)
+                    return left;
+                return left.getParent(value);
+            }
+        } catch (NullPointerException err) {
+            return null;
+        }
+        
+        System.out.println("Tried to find the top element's parent.");
+        return null;
+    }
+    
+    public void remove(int value) {
+        if (value == this.value) {
+            if (isLeaf()) {
+                try {
+                    getParent(value).remove(value);
+                } catch (NullPointerException err) {
+                    System.out.println("Can't remove the only element in the tree!");
+                }
+            } else {
+                try {
+                    int min_on_right = right.getMin();
+                    System.out.println("min_from " + right.value + " = " + min_on_right);
+                    remove(min_on_right);
+                    this.value = min_on_right;
+                } catch (NullPointerException err) {
+                    int max_on_left = left.getMax();
+                    System.out.println("max_from " + left.value + " = " + max_on_left);
+                    remove(max_on_left);
+                    this.value = max_on_left;
+                }
+            }
+        } else if (right != null && value == right.value) {
+            if (right.isLeaf())
+                right = null;
+            else
+                right.remove(value);
+        } else if (left != null && value == left.value) {
+            if (left.isLeaf())
+                left = null;
+            else
+                left.remove(value);
+        } else {
+            getParent(value).remove(value);
+        }
+    }
+    
+    public void rebalance() {
+        List<Integer> lst = serialise();
+        left = null;
+        right = null;
+        
+        Collections.sort(lst);
+        rebalance(lst);
+    }
+    
+    private void rebalance(List<Integer> lst) {
+        if (lst.size() == 0)
+            return;
+        
+        int median_index = lst.size()/2;
+        value = lst.get(median_index);
+        
+        Integer[] total_lst = lst.toArray(new Integer[0]);
+        //System.out.println("\ntotal_list = " + IOGeneric.listToString(total_lst, "[,]"));
+        
+        if (0 < median_index) {
+            left = new IntegerTreeNode();
+            //return values.toArray(new String[0]);
+            Integer[] left_lst = lst.subList(0, median_index).toArray(new Integer[0]);
+            System.out.println("\ntotal_list = "+IOGeneric.listToString(total_lst, "[,]")+ "   left_list = " + IOGeneric.listToString(left_lst, "[,]"));
+            left.rebalance(lst.subList(0, median_index));
+        }
+        if (median_index+1 < lst.size()) {
+            Integer[] left_lst = lst.subList(median_index+1, lst.size()).toArray(new Integer[0]);
+            System.out.println("\ntotal_list = "+IOGeneric.listToString(total_lst, "[,]")+ "   right_list = " + IOGeneric.listToString(left_lst, "[,]"));
+            right = new IntegerTreeNode();
+            right.rebalance(lst.subList(median_index+1, lst.size()));
+        }
+            
+    }
+    
+    public ArrayList<Integer> serialise() {
+        ArrayList<Integer> lst = new ArrayList<Integer>();
+        serialise(lst);
+        return lst;
+    }
+    
+    private void serialise(ArrayList<Integer> lst) {
+        lst.add(value);
+        if (left != null)
+            left.serialise(lst);
+        if (right != null)
+            right.serialise(lst);
     }
 }
--- src/exercise8/IntegerTreeNodeExercise.java
+++ src/exercise8/IntegerTreeNodeExercise.java
@@ -26,13 +26,22 @@ public class IntegerTreeNodeExercise implements Exercise {
     @Override
     public void run() {
         IntegerTreeNode top = new IntegerTreeNode(10);
-        top.add(5);
-        top.add(15);
-        top.add(14);
-        top.add(16);
+        top.add(10);
+        top.add(50);
+        top.add(40);
+        top.add(42);
+        top.add(41);
+        top.add(43);
+        top.add(60);
         
         System.out.format("Max is %d and Min is %d\n", top.getMax(), top.getMin());
         System.out.println("Tree is: " + top);
+        System.out.println("Tree depth is: " + top.depth());
+        top.remove(42);
+        System.out.println("Removed '42': " + top);
+        top.rebalance();
+        System.out.println("Rebalanced: " + top);
+        
     }
 
 }
